329b9f10bcf89e3bd3172fddb6253954b2e807ee
joel2064 <joel2064@gmail.com>
2024-10-01 19:45:55 -0500

    fallo 001

--- src/main/java/com/bezkoder/spring/test/repository/TutorialRepository.java
+++ src/main/java/com/bezkoder/spring/test/repository/TutorialRepository.java
@@ -10,4 +10,7 @@ public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
   List<Tutorial> findByPublished(boolean published);
 
   List<Tutorial> findByTitleContaining(String title);
+  
+  int valor = 1
+
 }
